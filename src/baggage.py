83f614db5602e61e2c4147b16fc932447035df59
KevinZasshu <kevinchantan0@gmail.com>
2026-05-23 16:10:00 +0700

    adding failing test for carry on

--- src/baggage.py
+++ src/baggage.py
@@ -1,2 +1,4 @@
-def validate_baggage():
-    return -1
+def validate_baggage(baggage_type):
+    max_weight = 0
+    if baggage_type == 'carry-on':
+        max_weight = 7
--- test_baggage.py
+++ test_baggage.py
@@ -1,2 +1,13 @@
 import pytest
 from src.baggage import validate_baggage 
+
+_FAILED_MESSAGES = {"overweight": "Weight surpasses limit.", 
+                     "invalid": "Data is invalid."}
+
+def test_carry_on():
+    assert validate_baggage(-1, "carry-on", "economy", "domestic", hazardous_item=False) == _FAILED_MESSAGES["invalid"]
+    assert validate_baggage(-1, "carry-on", "business", "domestic", hazardous_item=False) == _FAILED_MESSAGES["invalid"]
+    assert validate_baggage(7, "carry-on", "economy", "domestic", hazardous_item=False) == _SUCCESS_MESSAGES["domestic"]
+    assert validate_baggage(7, "carry-on", "business", "domestic", hazardous_item=False) == _SUCCESS_MESSAGES["domestic"]
+    assert validate_baggage(8, "carry-on", "economy", "domestic", hazardous_item=False) == _FAILED_MESSAGES["overweight"]
+    assert validate_baggage(8, "carry-on", "business", "domestic", hazardous_item=False) == _FAILED_MESSAGES["overweight"]
