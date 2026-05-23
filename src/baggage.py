b2f19390e95e17a3620fa156e8b31ce74e698f80
theodore.alexander001@binus.ac.id <theodore.alexander001@binus.ac.id>
2026-05-23 15:47:28 +0700

    refactor code validate baggage domestic or international

--- src/baggage.py
+++ src/baggage.py
@@ -1,11 +1,17 @@
 _SUCCESS_MESSAGES = {"domestic": "Checked in successful.", 
                         "international": "Checked in successful. Passport is required"}
 
-def validate_baggage(baggage_weight, baggage_type, passenger_class, flight_type, hazardous_item):
-    if flight_type == "international":
-        return _SUCCESS_MESSAGES["international"]
+def isDomestic(type):
+    if (type == "international"):
+        return False
     else:
+        return True
+
+def validate_baggage(baggage_weight, baggage_type, passenger_class, flight_type, hazardous_item):
+    if isDomestic(flight_type):
         return _SUCCESS_MESSAGES["domestic"]
+    else:
+        return _SUCCESS_MESSAGES["international"]
 
 
 
